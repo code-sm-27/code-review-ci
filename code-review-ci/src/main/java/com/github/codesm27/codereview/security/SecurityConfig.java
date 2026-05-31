f421566ccabf3f62fb9ddc6039cb3011171a6f01
Shivamani Burgu <shiva.burgu@gmail.com>
2026-05-31 16:09:21 +0530

    Added Vercel domain to CORS config

--- code-review-ci/src/main/java/com/github/codesm27/codereview/security/SecurityConfig.java
+++ code-review-ci/src/main/java/com/github/codesm27/codereview/security/SecurityConfig.java
@@ -45,7 +45,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173")); // Vite dev server
+        configuration.setAllowedOrigins(List.of("http://localhost:5173", "https://code-review-ci.vercel.app")); // Vite dev server & Vercel
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
